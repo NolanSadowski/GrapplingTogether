726cb832836982952b5d4f5530f17a8e8d6b42f8
NolanSadowski <47158785+NolanSadowski@users.noreply.github.com>
2020-05-22 23:05:59 -0400

    Added some scripy scripts
    music manger, screen shake, voice recognition

---
+++ SSDGJ/Assets/Scripts/ShakeBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeBehavior : MonoBehaviour
+{
+    // Transform of the GameObject you want to shake
+    private Transform camTransform;
+
+    // Desired duration of the shake effect
+    public static float shakeDuration = 0f;
+
+    // A measure of magnitude for the shake. Tweak based on your preference
+    private float shakeMagnitude = 0.25f;
+
+    // A measure of how quickly the shake effect should evaporate
+    private float dampingSpeed = 2.0f;
+
+    // The initial position of the GameObject
+    Vector3 initialPosition;
+
+    void Awake()
+    {
+        if (transform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    void OnEnable()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        if (shakeDuration > 0)
+        {
+            transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+
+            shakeDuration -= Time.deltaTime * dampingSpeed;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            transform.localPosition = initialPosition;
+        }
+    }
+}
---
+++ SSDGJ/Assets/Scripts/VoiceRecognition.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class VoiceRecognition : MonoBehaviour
+{
+    private KeywordRecognizer keyWordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    public float difficultyScale = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actions.Add("fuck", MakeHarder);
+        actions.Add("shit", MakeHarder);
+        actions.Add("shitting", MakeHarder);
+        actions.Add("bitch", MakeHarder);
+        actions.Add("ass", MakeHarder);
+        actions.Add("cunt", MakeHarder);
+        actions.Add("motherfucker", MakeHarder);
+        actions.Add("slut", MakeHarder);
+        actions.Add("hoe", MakeHarder);
+        actions.Add("whore", MakeHarder);
+        actions.Add("fucking", MakeHarder);
+        actions.Add("twat", MakeHarder);
+        actions.Add("pussy", MakeHarder);
+        actions.Add("dick", MakeHarder);
+        actions.Add("cock", MakeHarder);
+        actions.Add("cocksucker", MakeHarder);
+
+        keyWordRecognizer = new KeywordRecognizer(actions.Keys.ToArray(), ConfidenceLevel.Medium);
+        keyWordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keyWordRecognizer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void MakeHarder()
+    {
+        //increase difficulty here
+        difficultyScale += 1;
+    }
+}
+
