3cd3310a6b6b9dcdc5177e88ef05c3c47f110653
Buscems <46265895+Buscems@users.noreply.github.com>
2020-05-22 22:13:14 -0400

    Here we go

---
+++ SSDGJ/Assets/Scripts/MainPlayer.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using Rewired.ControllerExtensions;
+
+public class MainPlayer : MonoBehaviour
+{
+
+    //the following is in order to use rewired
+    [Tooltip("Reference for using rewired")]
+    [HideInInspector]
+    public Player player1;
+    public Player player2;
+    [Header("Rewired")]
+    [Tooltip("Number identifier for each player, must be above 0")]
+    public int[] playerNums;
+    public int playerNum;
+
+    [Header("Looking")]
+    public float sensitivity;
+
+    public Transform body;
+
+    float Xrotation = 0f;
+
+    bool usingController;
+
+    private void Awake()
+    {
+        //Rewired Code
+        player1 = ReInput.players.GetPlayer(playerNums[0]);
+        player2 = ReInput.players.GetPlayer(playerNums[1]);
+        ReInput.ControllerConnectedEvent += OnControllerConnected;
+        CheckController(player1);
+        CheckController(player2);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        Gravity();
+    }
+
+    private void FixedUpdate()
+    {
+        FixedMovement();
+    }
+
+    void Movement()
+    {
+        
+    }
+
+    void FixedMovement()
+    {
+        
+    }
+
+    void Gravity()
+    {
+        
+    }
+
+    //[REWIRED METHODS]
+    //these two methods are for ReWired, if any of you guys have any questions about it I can answer them, but you don't need to worry about this for working on the game - Buscemi
+    void OnControllerConnected(ControllerStatusChangedEventArgs arg)
+    {
+        CheckController(player1);
+        CheckController(player2);
+    }
+
+    void CheckController(Player player)
+    {
+        foreach (Joystick joyStick in player.controllers.Joysticks)
+        {
+            var ds4 = joyStick.GetExtension<DualShock4Extension>();
+            if (ds4 == null) continue;//skip this if not DualShock4
+            switch (playerNum)
+            {
+                case 4:
+                    ds4.SetLightColor(Color.yellow);
+                    break;
+                case 3:
+                    ds4.SetLightColor(Color.green);
+                    break;
+                case 2:
+                    ds4.SetLightColor(Color.blue);
+                    break;
+                case 1:
+                    ds4.SetLightColor(Color.red);
+                    break;
+                default:
+                    ds4.SetLightColor(Color.white);
+                    Debug.LogError("Player Num is 0, please change to a number > 0");
+                    break;
+            }
+        }
+    }
+
+}
